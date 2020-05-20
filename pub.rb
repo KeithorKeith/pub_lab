56fc552822dde4747f92ef24c23a02a4d376a1fd
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-20 13:30:52 +0100

    Added functionality for checking customer age.

--- pub.rb
+++ pub.rb
@@ -8,7 +8,15 @@ class Pub
         @drinks = drinks
     end
     
-    def sell_drink(drink)
-      @till += drink.price 
+    def sell_drink(drink, customer)
+        if customer.age >= 18
+            @till += drink.price
+        elsif customer.age < 18
+            return "Get out!"
+        end 
+    end
+
+    def check_age_of_customer(customer)
+      return customer.age()
     end
 end
--- pub_spec/pub_spec.rb
+++ pub_spec/pub_spec.rb
@@ -13,13 +13,27 @@ class PubTest < MiniTest::Test
     @drink_1 = Drink.new("Vodka", 10)
     @drink_2 = Drink.new("Gin", 10)
     @drink_3 = Drink.new("Rum", 10)
-    @customer = Customer.new("John", 100, "30")
+    @customer_1 = Customer.new("John", 100, 30)
+    @customer_2 = Customer.new("Brian", 30, 17)
   end
     
-  def test_pub_can_sell_drink()
-    @pub.sell_drink(@drink_1)
+  def test_pub_can_sell_drink__above_18()
+    @pub.sell_drink(@drink_1, @customer_1)
     assert_equal(10, @pub.till())
   end
 
+  def test_pub_can_sell_drink__under_18()
+    assert_equal("Get out!", @pub.sell_drink(@drink_2, @customer_2))
+  end
+
+  def test_check_age_of_customer()
+    @pub.check_age_of_customer(@customer_1)
+    assert_equal(30, @customer_1.age())
+  end
+
+  # def test_can_sell_if_age_above_18()
+  #   @pub.check_age_of_customer(@customer)
+  #   assert_equal(18, )
+  # end
 
 end
